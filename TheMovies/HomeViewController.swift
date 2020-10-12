afdc9d55736719e019f7bbf0422451eb74bf8096
mehmet koca <mehmetkoca@outlook.com>
2020-10-12 21:13:19 +0300

    Add table view in home view scene

--- TheMovies/HomeViewController.swift
+++ TheMovies/HomeViewController.swift
@@ -14,10 +14,18 @@ private enum Constant {
 
 final class HomeViewController: BaseViewController {
     
+    private let tableView = UITableView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViews()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        configureTableView()
     }
 }
 
@@ -27,5 +35,14 @@ private extension HomeViewController {
     
     func configureViews() {
         title = Constant.title
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 }
