c71e9df6ae37d7ed6015aca8e26d53143395ca9d
mehmet koca <mehmetkoca@outlook.com>
2020-10-17 19:22:01 +0300

    Add estimatedRowHeight value to Constant

--- TheMovies/Scenes/MovieDetails/MovieDetailsViewController.swift
+++ TheMovies/Scenes/MovieDetails/MovieDetailsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 private enum Constant {
     
     static let title = "Movie Details"
+    static let estimatedRowHeight: CGFloat = 200.0
     
     enum MovieDetails: Int {
         
@@ -65,7 +66,7 @@ private extension MovieDetailsViewController {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        tableView.estimatedRowHeight = 600
+        tableView.estimatedRowHeight = Constant.estimatedRowHeight
         tableView.rowHeight = UITableView.automaticDimension
         tableView.dataSource = self
         tableView.delegate = self
